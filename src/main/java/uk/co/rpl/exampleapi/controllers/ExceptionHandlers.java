15636f73644108e5429b77ba2be2f2b7464db408
philip <philip.robinson@r-pl.co.uk>
2022-06-09 10:45:51 +0100

    General exception handler added to trap any unspecified exceptions

--- src/main/java/uk/co/rpl/exampleapi/controllers/ExceptionHandlers.java
+++ src/main/java/uk/co/rpl/exampleapi/controllers/ExceptionHandlers.java
@@ -1,9 +1,10 @@
 /*
- * 
- * 
+ *
+ *
  */
 package uk.co.rpl.exampleapi.controllers;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -24,5 +25,14 @@ public class ExceptionHandlers {
                                     }
                                     """.replace("{msg}", e.getMessage()),
                                     NOT_FOUND);
+    }
+    @ExceptionHandler({Throwable.class})
+    public ResponseEntity<String> handleGenException(Throwable e){
+        return new ResponseEntity<>("""
+                                    {
+                                        "message": "{msg}"
+                                    }
+                                    """.replace("{msg}", e.getMessage()),
+                                    INTERNAL_SERVER_ERROR);
     }
 }
